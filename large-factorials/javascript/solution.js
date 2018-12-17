272aaf32ed3335e759efa801ed02af1b35cec70c
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-12-17 19:36:40 +0400

    Restore solution

--- large-factorials/javascript/solution.js
+++ large-factorials/javascript/solution.js
@@ -1,38 +1,30 @@
-const mulLimit = Math.sqrt(Number.MAX_SAFE_INTEGER);
-
 function mul(a, b) {
-  let ret = '0';
-  for (var i = 0; i < +a; i++) {
-    ret = sum(ret, b);
-  }
-  return ret;
-}
-
-function sum(a, b) {
   a = a + '';
   b = b + '';
-  var longest = a.length > b.length ? a : b;
-  var shortest = b === longest ? a : b;
+  var min;
+  if (a.length < b.length) {
+    min = a;
+  } else if (b.length < a.length) {
+    min = b;
+  } else {
+    min = a < b ? a : b;
+  }
+  var max = min === a ? b : a;
 
-  var lA = longest.split('').reverse();
-  var sA = shortest.split('').reverse();
+  var mA = max.split('').reverse();
 
-  var result = [];
   var remainder = 0;
-  for (let i = 0; i < lA.length; i++) {
-    var tmp = +lA[i] + (+sA[i] || 0) + remainder;
-    if (tmp < 10) {
-      remainder = 0;
-      result[i] = tmp;
-    } else {
-      remainder = 1;
-      result[i] = tmp % 10;
-    }
+  var ret = [];
+  for (var i = 0; i < mA.length; i++) {
+    var tmp = +mA[i] * min + remainder;
+    var val = tmp % 10;
+    remainder = (tmp - val) / 10;
+    ret.push(val);
   }
   if (remainder) {
-    result.push(remainder);
+    ret.push(remainder);
   }
-  return result.reverse().join('');
+  return ret.reverse().join('');
 }
 
 var factorialMemory = {};
